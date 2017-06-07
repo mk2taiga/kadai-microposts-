dfae90571393a83a840e94e31d947f5abf609d15
Taiga Miyazaki <mk2_tg000@icloud.com>
2017-06-07 05:30:29 +0000

    post

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+Route::get('/', 'WelcomeController@index');
 // ユーザ登録
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
@@ -10,8 +11,7 @@ Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
